5e92beac3d25cc1190bf737c97b291e437ae090b
Janusz Skonieczny <git.js@bravelabs.pl>
2019-01-01 12:58:18 +0100

    version one

--- src/ztm/cli.py
+++ src/ztm/cli.py
@@ -2,16 +2,60 @@
 
 """Console script for ztm."""
 
+# coding=utf-8
+# Copyright (c) 2018 Janusz Skonieczny
+
+import logging
+import os
+import pathlib
+
 import click
+import requests
+
+log = logging.getLogger(__name__)
 
 
-@click.command()
-def main(args=None):
-    """Console script for ztm."""
-    click.echo("Replace this message by putting your code into "
-               "ztm.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.group()
+@click.option('--apikey', default=None)
+@click.pass_context
+def main(ctx, apikey):
+    ctx.ensure_object(dict)
+    ctx.obj['APIKEY'] = apikey or os.environ.get('APIKEY', default=None) or '58013e79-70ba-427d-be89-0bae95c53d40'
 
 
-if __name__ == "__main__":
-    main()
+@main.command()
+@click.option('--line', '-l', multiple=True)
+@click.pass_context
+def fetch(ctx, line):
+    """Pobierz dane o pozycji wskazanej linii"""
+    for item in line:
+        click.echo('Fetching position for %s!' % item)
+        data = {
+            'resource_id': 'f2e5503e927d-4ad3-9500-4ab9e55deb59',
+            'apikey': ctx.obj['APIKEY'],
+            'type': 1,
+            'line': item,
+        }
+        url = "https://api.um.warszawa.pl/api/action/busestrams_get/"
+        response = requests.get(url, params=data)
+        data = response.json()
+        click.echo(data)
+        append_data(data['result'])
+
+
+def append_data(data):
+    import csv
+
+    path = pathlib.Path('ztm.csv')
+    data_file_exists = path.exists()
+    fieldnames = ['Time', 'Lines', 'Brigade', 'Lat', 'Lon']
+    with path.open('a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if not data_file_exists:
+            writer.writeheader()
+        for item in data:
+            writer.writerow(item)
+
+
+if __name__ == '__main__':
+    main(obj={})
--- tasks.py
+++ tasks.py
@@ -19,10 +19,14 @@ import logging
 import os
 import shutil
 import sys
+import webbrowser
+from collections import OrderedDict
 from itertools import chain
 from pathlib import Path
 
 # noinspection PyPackageRequirements
+from urllib.request import pathname2url
+
 from invoke import call, task
 
 logging.basicConfig(format='%(asctime)s %(levelname)-7s %(thread)-5d %(filename)s:%(lineno)s | %(funcName)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -34,50 +38,86 @@ log = logging.getLogger(__name__)
 
 is_win = sys.platform == 'win32'
 ROOT_DIR = Path(__file__).parent.absolute()
-SRC_DIR = ROOT_DIR / 'src'
-VENV_DIR = ROOT_DIR / ".pve"
-VENV_BIN = VENV_DIR / ("Scripts" if is_win else "bin")
-PYTHON = VENV_BIN / 'python'
-PIP = VENV_BIN / 'pip'
-MANAGE = '{} {} '.format(PYTHON, SRC_DIR / 'manage.py')
+
+
+def get_current_version():
+    from configparser import ConfigParser
+    cfg = ConfigParser()
+    cfg.read(str(Path(ROOT_DIR) / 'setup.cfg'))
+    current_version = cfg.get('bumpversion', 'current_version')
+    return current_version
+
+
+# noinspection PyUnusedLocal
+@task
+def version(ctx):
+    print("Version: " + get_current_version())
 
 
 @task
 def clean(ctx):
+    """Remote temporary files"""
     for item in chain(Path(ROOT_DIR).rglob("*.pyc"), Path(ROOT_DIR).rglob("*.pyo")):
         logging.debug("Deleting: %s", item)
         item.unlink()
 
-    for item in Path(ROOT_DIR).rglob("__pycache__"):
-        logging.debug("Deleting: %s", item)
-        shutil.rmtree(str(item), ignore_errors=True)
+    log.info("Removing __pycache__ in sys.path folders")
+    for folder in sys.path:
+        for item in Path(folder).rglob("__pycache__"):
+            logging.debug("Deleting: %s", item)
+            shutil.rmtree(str(item), ignore_errors=True)
 
-    shutil.rmtree(str(ROOT_DIR / 'build'), ignore_errors=True)
-    shutil.rmtree(str(ROOT_DIR / 'example_project' / '.eggs'), ignore_errors=True)
-    shutil.rmtree(str(ROOT_DIR / '.eggs'), ignore_errors=True)
-    shutil.rmtree(str(ROOT_DIR / '.tox'), ignore_errors=True)
-    shutil.rmtree(str(ROOT_DIR / '.tmp'), ignore_errors=True)
-    shutil.rmtree(str(ROOT_DIR / '.coverage'), ignore_errors=True)
-    shutil.rmtree(str(ROOT_DIR / '.htmlcov'), ignore_errors=True)
-    shutil.rmtree(str(ROOT_DIR / '.pytest_cache'), ignore_errors=True)
-    shutil.rmtree(str(ROOT_DIR / '.cache'), ignore_errors=True)
+    folders = (
+        ROOT_DIR / 'build',
+        ROOT_DIR / 'example_project' / '.eggs',
+        ROOT_DIR / '.eggs',
+        ROOT_DIR / '.tox',
+        ROOT_DIR / '.tmp',
+        ROOT_DIR / '.coverage',
+        ROOT_DIR / '.htmlcov',
+        ROOT_DIR / '.pytest_cache',
+        ROOT_DIR / '.cache',
+    )
+
+    for folder in folders:
+        print("Removing folder {}".format(folder))
+        shutil.rmtree(str(folder), ignore_errors=True)
+
     ctx.run('git checkout -- .tmp')
 
 
 @task
-def lint(ctx):
+def check(ctx):
     """Check project codebase cleanness"""
     ctx.run("flake8 src tests setup.py manage.py")
     ctx.run("isort --check-only --diff --recursive src tests setup.py")
     ctx.run("python setup.py check --strict --metadata --restructuredtext")
     ctx.run("check-manifest  --ignore .idea,.idea/* .")
+    ctx.run("pytest --cov=src --cov=tests --cov-fail-under=5")
+
+
+@task
+def coverage(ctx):
+    ctx.run("pytest --cov=src --cov=tests --cov-fail-under=5 --cov-report html")
+    webbrowser.open("file://" + pathname2url(str(ROOT_DIR / '.tmp' / 'coverage' / 'index.html')))
+
+
+@task
+def isort(ctx):
+    """Check project codebase cleanness"""
+    ctx.run("isort --recursive src tests setup.py")
 
 
 @task
 def detox(ctx):
+    """Run detox with a subset of envs and report run separately"""
     envs = ctx.run("tox -l").stdout.splitlines()
+    envs.remove('clean')
     envs.remove('report')
-    ctx.run("detox -e " + ",".join(env for env in envs[:-1]))
+    envs = [e for e in envs if not e.startswith('py2')]
+    log.info("Detox a subset of environments: %s", envs)
+    ctx.run("tox -e clean")
+    ctx.run("detox --skip-missing-interpreters -e " + ",".join(envs))
     ctx.run("tox -e report")
 
 
@@ -104,6 +144,7 @@ def upload_pypi(ctx):
 
 @task(clean)
 def dist(ctx):
+    """Build setuptools dist package"""
     ctx.run("python setup.py sdist")
     ctx.run("python setup.py bdist_wheel")
     ctx.run("ls -l dist")
@@ -111,6 +152,7 @@ def dist(ctx):
 
 @task(clean)
 def install(ctx):
+    """Install setuptools dist package"""
     ctx.run("python setup.py install")
 
 
@@ -126,185 +168,79 @@ def sync(ctx):
     ctx.run("git checkout develop")
     ctx.run("git merge master --verbose")
 
-    ctx.run("git checkout master")
-    ctx.run("git merge develop --verbose")
-
     ctx.run("git checkout develop")
 
 
-@task
-def bump(ctx):
-    """Increment version number"""
-    ctx.run("bumpversion patch --no-tag")
+@task(sync)
+def sync_master(ctx):
+    ctx.run("git checkout master")
+    ctx.run("git merge develop --verbose")
 
-
-@task()
-def upgrade(ctx):
-    """Upgrade frozen requirements to the latest version"""
-    ctx.run('pip-compile requirements.in -o requirements.txt --verbose --upgrade')
-    ctx.run('sort requirements.txt -o requirements.txt')
-    with (ROOT_DIR / 'requirements.txt').open('a') as f:
-        f.write('--find-links=requirements')
-    ctx.run('git add requirements.txt')
-    ctx.run('git commit -m "Requirements upgrade"')
-
-
-@task
-def assets(ctx):
-    """
-    Collect and build website assets
-    """
-    # with ctx.cd('../ztm-styles'):
-    #     ctx.run("gulp release")
-    ctx.run("python manage.py collectstatic --noinput")  # in some cases assets build requires static to be updated
-    ctx.run("python manage.py assets build")
-    ctx.run("python manage.py collectstatic --noinput")  # update static with compressed assets
-    ctx.run("git add --all static assets", warn=True)
-    ctx.run('git commit -m "Build assets"', warn=True)
-
-
-# noinspection PyUnusedLocal
-@task(lint, sync, bump)
-def release(ctx):
-    """Build new package version release and sync repo"""
     ctx.run("git checkout develop")
     ctx.run("git merge master --verbose")
 
     ctx.run("git push origin develop --verbose")
     ctx.run("git push origin master --verbose")
+    ctx.run("git push --follow-tags")
+
+
+@task()
+def bump(ctx):
+    """Increment version number"""
+    # ctx.run("bumpversion patch --no-tag")
+    ctx.run("bumpversion patch")
+
+
+@task()
+def pip_compile(ctx):
+    """Upgrade frozen requirements to the latest version"""
+    ctx.run('pip-compile requirements/production.txt -o requirements/lock/production.txt --verbose --upgrade')
+    ctx.run('sort requirements/lock/production.txt -o requirements/lock/production.txt')
+    ctx.run('git add requirements/lock/*.txt')
+    if ctx.run('git diff-index --quiet HEAD', warn=True).exited != 0:
+        ctx.run('git commit -m "Requirements compiled by pip-compile" --allow-empty')
+
+
+@task()
+def pipenv(ctx):
+    """Upgrade frozen requirements to the latest version"""
+    ctx.run('pipenv install -r requirements/production.txt')
+    ctx.run('pipenv install --dev -r requirements/development.txt')
+    ctx.run('pipenv lock --requirements > requirements/lock/production.txt')
+    ctx.run('pipenv lock --requirements --dev | grep -v "/multiinfo-python" -- > requirements/lock/development.txt')
+    ctx.run('pipenv graph --reverse -- > requirements/lock/graph.txt')
+    ctx.run('sort requirements/lock/production.txt -o requirements/lock/production.txt')
+    ctx.run('sort requirements/lock/development.txt -o requirements/lock/development.txt')
+    ctx.run('git add Pipfile Pipfile.lock requirements/lock/*.txt')
+    ctx.run('git commit -m "Requirements locked by pipenv"')
 
 
 # noinspection PyUnusedLocal
-@task(release, upload_pypi)
+@task(check, sync, detox)
+def release_start(ctx):
+    """Start a release cycle with publishing a release branch"""
+    ctx.run("git flow release start v{}-release".format(get_current_version()))
+    ctx.run("git merge master --verbose")
+    ctx.run("bumpversion patch --no-tag --verbose ")
+    ctx.run("git flow release --verbose publish")
+
+
+# noinspection PyUnusedLocal
+@task(check, sync, detox, post=[])
+def release_finish(ctx):
+    """Finish a release cycle with publishing a release branch"""
+    ctx.run("git flow release finish --fetch --push")
+
+
+# noinspection PyUnusedLocal
+@task(isort, check, pip_compile, sync, detox, bump, sync_master)
+def release(ctx):
+    """Build new package version release and sync repo"""
+
+
+# noinspection PyUnusedLocal
+@task(release, post=[upload_pypi])
 def publish(ctx):
-    """Release and upload new version"""
-
-
-@task
-def locales(ctx):
-    """
-    Collect and compile translation strings
-    """
-    # https://docs.djangoproject.com/en/dev/ref/django-admin/#django-admin-makemessages
-    """
-    python manage.py makemessages -v 3 --no-wrap --ignore ".*" --locale=pl_PL
-    python manage.py compilemessages -v 3
-    """
-    tmp = ROOT_DIR / ".tmp"
-    if not tmp.exists():
-        os.makedirs(str(tmp))
-    locale = ROOT_DIR / "src" / "locale"
-    if not locale.exists():
-        os.makedirs(str(locale))
-    # http://babel.edgewall.org/wiki/BabelDjango
-    pybabel = str(VENV_BIN / 'pybabel')
-    ctx.run(pybabel + " extract -F locale/babel.cfg -o locale/django.pot --no-wrap --sort-output .")
-    # create locales firs
-    # pybabel init -D django -i locale/django.pot -d locale -l es
-    # http://babel.edgewall.org/wiki/BabelDjango#CreatingandUpdatingTranslationsCatalogs
-    # ctx.run(pybabel + " update -D django -i locale/django.pot -d locale --ignore-obsolete")
-    ctx.run(pybabel + " update -D django -i locale/django.pot -d locale --previous --no-wrap")
-    ctx.run(pybabel + " compile -D django -d locale --statistics")
-    log.info("JavaScript locales")
-    # ctx.run(pybabel + " update -D djangojs -i locale/djangojs.pot -d locale --previous --no-wrap")
-    ctx.run("django-admin makemessages -d djangojs -i static -i node_modules")
-    ctx.run(pybabel + " compile -D djangojs -d locale --statistics")
-
-
-@task
-def trigger_tests(ctx):
-    print(" #### Signaling test repo")
-    ingeration_testing_root = str(Path(ROOT_DIR).parent / "integration-testing")
-    print(ingeration_testing_root)
-    env = {
-        'GIT_WORK_TREE': ingeration_testing_root,
-        'GIT_DIR': str(Path(ingeration_testing_root) / '.git'),
-    }
-    from configparser import ConfigParser
-    cfg = ConfigParser()
-    cfg.read(str(Path(ROOT_DIR) / '.bumpversion.cfg'))
-    current_version = cfg.get('bumpversion', 'current_version')
-    ctx.run("git checkout develop", env=env)
-    cmd = 'git commit --allow-empty -m "Test release {}"'.format(current_version)
-    print(cmd)
-    ctx.run(cmd, env=env)
-    ctx.run("git push origin develop", env=env)
-
-
-@task(pre=[release], post=[])
-def deploy(ctx, remote='dev', branch='master'):
-    """
-    Collect and compile assets, add, commit and push to remote
-    """
-    ctx.run("git checkout {branch}".format(branch=branch))
-    ctx.run("git push {remote} {branch}  --verbose".format(remote=remote, branch=branch))
-    ctx.run("heroku logs -r {remote}".format(remote=remote))  # We need this to show release script output
-    ctx.run("git checkout develop")
-
-
-# noinspection PyUnusedLocal
-@task(pre=[call(deploy, remote='production', branch='master')])
-def deploy_production(ctx):
-    pass
-
-
-# noinspection PyUnusedLocal
-@task(pre=[call(deploy, remote='staging', branch='master')])
-def deploy_staging(ctx):
-    pass
-
-
-@task
-def vagrant(ctx):
-    """
-    Collect and compile assets, add, commit and push to production remote
-    """
-    ctx.run("git checkout develop")
-    ctx.run("git push vagrant develop --verbose")
-
-
-FIXTURES = {
-    "auth": (
-        "auth.Group",
-        "auth.User",
-    ),
-}
-
-
-@task
-def dump(ctx):
-    for file, what in FIXTURES.items():
-        log.info("Dumping fixture: %s" % file)
-        cmd = "python manage.py dumpdata --format=yaml --natural-foreign --natural-primary {} > " + str(ROOT_DIR / 'fixtures' / '{}.yaml')
-        cmd = cmd.format(" ".join(what), file)
-        log.debug(cmd)
-        ctx.run(cmd)
-
-
-@task
-def reset_db(ctx):
-    data = ROOT_DIR / 'data'
-    for p in data.glob("db.*"):
-        os.remove(str(p))
-    if not data.exists():
-        os.makedirs(str(data))
-    ctx.run("python manage.py migrate")
-
-
-@task
-def load_fixtures(ctx):
-    for fixture in FIXTURES.keys():
-        fixture = str(ROOT_DIR / 'fixtures' / (fixture + '.yaml'))
-        log.info("Dumping fixture: %s" % fixture)
-        cmd = "python manage.py loaddata " + fixture
-        logging.debug(cmd)
-        ctx.run(cmd)
-
-
-@task(pre=[reset_db, load_fixtures])
-def db(ctx):
-    """
-    Full database re-initialization
-    """
-    cmd = "python manage.py fill_test_data "
-    ctx.run(cmd)
+    """Merge develop, create and upload new version"""
+    ctx.run("git checkout master")
+    ctx.run("git merge develop --verbose")
